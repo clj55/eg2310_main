67831d2c2925e50c3c75a5d4dcac70b619c272b1
WAAAJIAN <157587457+WAAAJIAN@users.noreply.github.com>
2024-03-29 12:30:46 +0800

    finalised the ESP32.py code
    tested the code here and successfully activated the esp32

--- autonomous_exploration/Rpi->esp32_test.py
+++ autonomous_exploration/Rpi->esp32_test.py
@@ -1,12 +1,13 @@
 import requests
-esp32_ip = '172.20.10.4'
-turtleBot_ID = 43
+import json
 
-endpoint = "http://" + esp32_ip + "/openDoor"
-data = {"action": "openDoor", "parameters": {"robotId": turtleBot_ID}}
-header = {'Content-Type': 'application/json'}
+esp32_ip = '172.20.10.5' # this ip changes everytime you use it, but the ip wont change depending on the computer
+turtleBot_ID = '43' # turtlebot3 burger ID we receive on the first day
 
-requests.post(endpoint, params=data, headers=header)
-response = requests.get(endpoint, params=data, headers=header)
+endpoint = "http://" + esp32_ip + "/openDoor" # url to connect to the server of esp32, which then goes into the directory "openDoor"
+data = {"action": "openDoor", "parameters": {"robotId": turtleBot_ID}} # the messages the esp32 checks to see if it should open the door
+header = {'Content-Type': 'application/json'} # tells the esp32 what type of file we are sending
 
-print(response)
+response = requests.post(endpoint, json=data, headers=header) # writes the output while sends the http request to the esp32
+
+print(response.text) # printing outcome
