4bf258f70d2a03eda5204c996697881ebcab7667
WAAAJIAN <157587457+WAAAJIAN@users.noreply.github.com>
2024-03-31 14:58:18 +0800

    Update Rpi->esp32_test.py
    grammar error

--- autonomous_exploration/Rpi->esp32_test.py
+++ autonomous_exploration/Rpi->esp32_test.py
@@ -5,7 +5,7 @@ esp32_ip = '172.20.10.5' # this ip changes when you switch locations??, but the 
 turtleBot_ID = '43' # turtlebot3 burger ID we receive on the first day
 
 endpoint = "http://" + esp32_ip + "/openDoor" # url to connect to the server of esp32, which then goes into the directory "openDoor"
-data = {"action": "openDoor", "parameters": {"robotId": turtleBot_ID}} # the messages the esp32 checks to see if it should open the door
+data = {"action": "openDoor", "parameters": {"robotId": turtleBot_ID}} # this is a message for the esp32 to check if it should open the door
 header = {'Content-Type': 'application/json'} # tells the esp32 what type of file we are sending
 
 response = requests.post(endpoint, json=data, headers=header) # writes the output while sends the http request to the esp32
